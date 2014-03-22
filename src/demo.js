4e048e0d4472cf3b9b1fe8bfa5a9fdb47298516b
ismael <ismaelmgr@gmail.com>
2014-03-22 15:53:18 +0100

    Reestructurados personajes e implementado el cargado/guardado

--- src/demo.js
+++ src/demo.js
@@ -1,13 +1,40 @@
-names = [];
+//names = [];
 var MAX_ALLIES = 4, MAX_ENEMIES = 6;
 var contAllies = 0, contEnemies = 0;
-characters = {};
+characters = [];
+
+saveCharacters = function(){
+	var serialization = JSON.stringify(characters);
+	var dataurl = "data:application/javascript;ucs2,"+ serialization;
+	window.open(dataurl);
+}
+
+loadCharacters = function(){
+	var filereader = new FileReader();
+	
+	filereader.onloadend = function (){
+		characters = [];
+		characters = JSON.parse(filereader.result);
+		contAllies = 0;
+		contEnemies = 0;
+		for (var i = 0; i < characters.length; i++){
+			if (characters[i].faction == "ally")
+				contAllies++;
+			else
+				contEnemies++;
+		}
+		
+	}
+	var file = document.getElementById("fileUpload").files[0];
+	filereader.readAsText(file,'utf8');
+}
 
 newCharacter = function () {
 	var characterFaction = prompt("ally or enemy");
 	
 	if (characterFaction == "ally" || characterFaction == "enemy") {
 		var valid = false;
+		
 		if (contAllies < MAX_ALLIES && characterFaction == "ally") {
 			contAllies++;
 			valid = true;
@@ -18,11 +45,14 @@ newCharacter = function () {
 		} else {
 			alert("Too many characters of this faction!")
 		}
+		
+		
 		if (valid) {
 			var name = prompt("Name of this Character");
 			
-			names[contAllies+contEnemies-1] = name;
-			characters[name] = {
+		//	names[contAllies+contEnemies-1] = name;
+			characters[contAllies+contEnemies-1] = {
+					name: name,
 					strength: parseInt(prompt("Initial Strength")),
 					agility: parseInt(prompt("Initial Agility")),
 					inteligence: parseInt(prompt("Initial Inteligence")),
@@ -39,11 +69,13 @@ newCharacter = function () {
 		alert("ally or enemy please");
 	}
 };
+
 fightersGenerator = function (){
 	for (var fighterAlly = 1; contAllies < MAX_ALLIES; contAllies++, fighterAlly++){
 		var name = "Ally" + fighterAlly;
-		names[contAllies] = name;
-		characters[name] = {
+	//	names[contAllies] = name;
+		characters[contAllies] = {
+				name: name,
 				strength: 1,
 				agility: 1,
 				inteligence: 1,
@@ -56,10 +88,12 @@ fightersGenerator = function (){
 		};
 		calcSubAttributes();
 	}
+	
 	for (var fighterEnemy = 1; contEnemies < MAX_ENEMIES; contEnemies++, fighterEnemy++){
 		var name = "Enemy" + fighterEnemy;
-		names[MAX_ALLIES + contEnemies] = name;
-        characters[name] = {
+//		names[MAX_ALLIES + contEnemies] = name;
+        characters[MAX_ALLIES + contEnemies] = {
+        	name: name,
     		strength: 1,
 			agility: 1,
 			inteligence: 1,
@@ -73,9 +107,10 @@ fightersGenerator = function (){
         calcSubAttributes();
 	}
 };
+
 modifyAttributes = function () {
-	var searchFighter = prompt("How do you want to change?"), contSearch = 0, found = true;
-	while (searchFighter != names[contSearch] && found) {
+	var searchFighter = prompt("Which character do you want to change?"), contSearch = 0, found = true;
+	while (searchFighter != characters[contSearch].name && found) {
 		if (contSearch > MAX_ALLIES + MAX_ENEMIES) { //Control de ERRORES en el caso de que no encuentre nada
 			found = false;
 		}		
@@ -84,33 +119,35 @@ modifyAttributes = function () {
 	
 	if (found) {
 		modAttr = prompt("What Attribute do you want to modify? strength, agility or inteligence");
-		characters[names[contSearch]][modAttr] = parseInt(prompt("Insert the New Value"));
-		console.log(modAttr + " for " + names[contSearch] + " is now " + characters[names[contSearch]][modAttr]);
+		characters[contSearch][modAttr] = parseInt(prompt("Insert the New Value"));
+		console.log(modAttr + " for " + characters[contSearch].name + " is now " + characters[contSearch][modAttr]);
 	} else {
 		console.log("Fighter Not Found!")
 	}
 	calcSubAttributes();
 };
+
 calcSubAttributes = function (){ // Función a la que llamaremos para refrescar los subatributos
 	var contCalcSub = 0;
-	while (contCalcSub < names.length) {
-		characters[names[contCalcSub]].hp = characters[names[contCalcSub]].strength * 3;
+	while (contCalcSub < characters.length) {
+		characters[contCalcSub].hp = characters[contCalcSub].strength * 3;
 		contCalcSub++;
 	}
 	showInfoFighters();
 };
+
 showInfoFighters = function(){
 	items = document.getElementById("infoFightersContent");
 	while (items.hasChildNodes()) {
 		items.removeChild(items.firstChild);
 	}
-	for (var i = 0; i < names.length; i++) {		
+	for (var i = 0; i < characters.length; i++) {		
 		
 		var item = document.createElement("li");
-		item.innerHTML = names[i];
+		item.innerHTML = characters[i].name;
 		
 		var str = document.createElement("ul");
-		str.innerHTML = "Strength: " + characters[names[i]].strength;
+		str.innerHTML = "Strength: " + characters[i].strength;
 		item.appendChild(str);
 		
 		items.appendChild(item);
